17562615e164da1c499d7674258219890da00725
costaivo <costa_ivo@yahoo.com>
2016-02-26 11:28:19 +0530

    Added configuration files

--- src/ClientManagerApp/src/ClientManagerApp/Startup.cs
+++ src/ClientManagerApp/src/ClientManagerApp/Startup.cs
@@ -11,21 +11,34 @@ namespace ClientManagerApp
 {
     public class Startup
     {
+        private IHostingEnvironment hostingEnvironment;
+
+
+        public Startup(IHostingEnvironment hostingEnvironment)
+        {
+            this.hostingEnvironment = hostingEnvironment;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            if (hostingEnvironment.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+
             app.UseIISPlatformHandler();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+
+            app.UseMvcWithDefaultRoute();
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
         }
 
         // Entry point for the application.
