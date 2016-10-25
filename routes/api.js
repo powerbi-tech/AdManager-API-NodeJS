8793e3c004632dcd12328e651f45ac39fba549d7
Ivo Costa <ivo@CreativeCapsule.local>
2016-10-25 21:12:00 +0530

    added routes for client

--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,7 @@ var router = express.Router();
 // Models
 var Product = require('../models/product');
 var Publication = require('../models/publication');
-
+var Client = require('../models/client');
 
 //Register  Routes
 Product.methods(['get','put','post','delete']);
@@ -15,6 +15,11 @@ Product.register(router,'/products');
 //Register  Routes for Publication Collection
 Publication.methods(['get','put','post','delete']);
 Publication.register(router,'/publications');
+
+
+//Register  Routes for Client Collection
+Client.methods(['get','put','post','delete']);
+Client.register(router,'/client');
 
 // Return router
 module.exports = router;
