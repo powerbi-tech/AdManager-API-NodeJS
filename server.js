d93d3a18870f3992791b8720db5c4ffda4ed9b7e
Ivo Costa <costa_ivo@yahoo.com>
2017-10-03 16:27:09 +0530

    Fixed Bugs

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 //MongoDB
-mongoose.connect('mongodb://localhost/rest_test');
+mongoose.connect('mongodb://dct:paradox@ds161304.mlab.com:61304/digitalads');
 
 
 //Express setup
 var app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
 //Routes
-app.use('/api',require('./routes/api'));
+app.use('/api', require('./routes/api'));
 
 //start the server
 app.listen(3000);
