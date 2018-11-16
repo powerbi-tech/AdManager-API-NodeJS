7d2decb3701a31858266c30e3f0f9fa11fce1685
Ivo Costa <costa_ivo@yahoo.com>
2018-11-17 00:34:38 +0530

    added post module

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,10 +1,12 @@
 import userRoutes from './users/user.routes'
 import { authJwt } from '../services/auth.services'
 import publicationRoutes from './publications/publication.routes'
+import postRoutes from './posts/post.routes'
 
 export default app => {
   app.use('/api/v1/users', userRoutes)
   app.use('/api/v1/publications', publicationRoutes)
+  app.use('/api/v1/posts', postRoutes)
   app.get('/hello', authJwt, (req, res) => {
     res.send('This is a private route!!!!')
   })
