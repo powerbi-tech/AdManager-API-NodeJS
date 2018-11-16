b0e218b5dc66f7f7e86bef47d124102e12bc01fb
Ivo Costa <costa_ivo@yahoo.com>
2018-11-16 23:35:31 +0530

    configured middleware

---
+++ src/helpers/logger.js
@@ -0,0 +1,17 @@
+import chalk from 'chalk';
+/* ToDo: Improvements 
+   Consider including modules like https://getpino.io/#/ or winston
+*/
+
+export function success(message) {
+  console.log(chalk.bold.green(`SUCCESS:${message}`));
+}
+
+export function info(message) {
+  console.log(chalk.bold.blue(`INFO:${message}`));
+}
+
+
+export function error(message) {
+    console.log(chalk.bold.red(`ERROR:${message}`));
+  }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
+// load external modules
 import express from 'express'
 
 // Custom imports
 import constants from './config/constants'
+import middlewareConfig from './config/middlewares'
+import apiRoutes from './modules'
+import './config/database'
+import * as logger from './helpers/logger'
 
 const app = express()
 const PORT = constants.PORT
 
 //Configure the middleware
+middlewareConfig(app)
 
 //Default Route to inform the user about the service
 app.get('/', (req, res) => {
-  res.send('Ad Manager : API')
+  res.send('Ad Manager : API Services are Running Successfully!')
 })
+
+apiRoutes(app)
 
 app.listen(PORT, err => {
   if (err) {
     throw err
   } else {
-    console.log(`Server running on port:${PORT} 
+    logger.info(`Server running on port:${PORT} 
         --> Running on ${process.env.NODE_ENV}
         --> Ready To Serve!!!....`)
   }
