d65e96bd778a14a9df35717a723f08ba8f7925c9
Ivo Costa <costa_ivo@yahoo.com>
2018-12-07 00:58:00 +0530

    Updated Routes

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,13 +2,24 @@ import userRoutes from './users/user.routes'
 import { authJwt } from '../services/auth.services'
 import publicationRoutes from './publications/publication.routes'
 import classifiedHeaderCaptionRoutes from './classifieds/classifiedHeaderCaptions/classifiedHeaderCaptions.routes'
+import classifiedRatesRoutes from './classifieds/ClassifiedRates/ClassifiedRate.routes'
 import clientRoutes from './clients/client.routes'
 import postRoutes from './posts/post.routes'
 
+const BASE_URL = '/api/v1'
+const CLASSIFIED = `${BASE_URL}/classifieds`
+
 export default app => {
-  app.use('/api/v1/users', userRoutes)
-  app.use('/api/v1/publications', publicationRoutes)
-  app.use('/api/v1/clients', clientRoutes)
-  app.use('/api/v1/classifiedHeaderCaptionRoutes', clientRoutes)
-  app.use('/api/v1/posts', postRoutes)
+  /* Common */
+  app.use(`${BASE_URL}/users`, userRoutes)
+  app.use(`${BASE_URL}/publications`, publicationRoutes)
+  app.use(`${BASE_URL}/clients`, clientRoutes)
+
+  /* Ads */
+
+  /* Classifieds */
+  app.use(`${CLASSIFIED}/Rates`, classifiedRatesRoutes)
+  app.use(`${CLASSIFIED}/HeaderCaptionRoutes`, classifiedHeaderCaptionRoutes)
+
+  app.use(`${BASE_URL}/posts`, postRoutes)
 }
