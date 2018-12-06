89236432724ee4672bbf9b380737fbc3801b1830
Ivo Costa <costa_ivo@yahoo.com>
2018-12-07 00:23:41 +0530

    Renamed to generic model name

--- src/modules/classifieds/classifiedHeaderCaptions/classifiedHeaderCaption.controllers.js
+++ src/modules/classifieds/classifiedHeaderCaptions/classifiedHeaderCaption.controllers.js
@@ -1,30 +1,30 @@
-import Client from './classifiedHeaderCaptions.model'
+import Model from './classifiedHeaderCaptions.model'
 import BaseController from '../core/base.controller'
 
 export default class ClientController extends BaseController {
   static async addNewRecord(req, res) {
     console.log(req.body)
-    super.addNewRecord(req, res, Client)
+    super.addNewRecord(req, res, Model)
   }
 
   static async getById(req, res) {
-    super.getById(req, res, Client)
+    super.getById(req, res, Model)
   }
 
   static async getList(req, res) {
-    super.getList(req, res, Client, '')
+    super.getList(req, res, Model, '')
   }
   static async getListWithParams(req, res) {
-    super.getListWithParams(req, res, Publication)
+    super.getListWithParams(req, res, Model)
   }
 
   static async updateRecord(req, res) {
-    super.updateRecord(req, res, Client)
+    super.updateRecord(req, res, Model)
   }
   static async deleteRecord(req, res) {
-    super.deleteRecord(req, res, Client)
+    super.deleteRecord(req, res, Model)
   }
   static async bulkUpdateRecords(req, res) {
-    super.bulkUpdateRecords(req, res, Client)
+    super.bulkUpdateRecords(req, res, Model)
   }
 }
