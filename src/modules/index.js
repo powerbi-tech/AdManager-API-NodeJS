f3fad4e1df174fa00dbdabb26f2df349ac371070
Ivo Costa <costa_ivo@yahoo.com>
2018-12-07 00:04:34 +0530

    added routes for classifiedHeaderCaption

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,7 @@
 import userRoutes from './users/user.routes'
 import { authJwt } from '../services/auth.services'
 import publicationRoutes from './publications/publication.routes'
+import classifiedHeaderCaptionRoutes from './classifieds/classifiedHeaderCaptions/classifiedHeaderCaptions.routes'
 import clientRoutes from './clients/client.routes'
 import postRoutes from './posts/post.routes'
 
@@ -8,8 +9,6 @@ export default app => {
   app.use('/api/v1/users', userRoutes)
   app.use('/api/v1/publications', publicationRoutes)
   app.use('/api/v1/clients', clientRoutes)
+  app.use('/api/v1/classifiedHeaderCaptionRoutes', clientRoutes)
   app.use('/api/v1/posts', postRoutes)
-  app.get('/hello', authJwt, (req, res) => {
-    res.send('This is a private route!!!!')
-  })
 }
