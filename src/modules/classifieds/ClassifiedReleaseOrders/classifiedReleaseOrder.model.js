19a047521d4012eab2659d27c8693ccbb4233d40
Ivo Costa <costa_ivo@yahoo.com>
2018-12-12 20:38:38 +0530

    Updated ClassifiedReleaseOrderSchema

--- src/modules/classifieds/ClassifiedReleaseOrders/classifiedReleaseOrder.model.js
+++ src/modules/classifieds/ClassifiedReleaseOrders/classifiedReleaseOrder.model.js
@@ -1,8 +1,9 @@
 import mongoose, { Schema } from 'mongoose'
 import auditingSchema from '../../core/base.model'
 import mongoosePaginate from 'mongoose-paginate'
+import uniqueValidator from 'mongoose-unique-validator'
 
-const ClassifiedRateSchema = new auditingSchema({
+const ClassifiedReleaseOrderSchema = new auditingSchema({
   publicationId: {
     type: Schema.ObjectId,
     ref: 'Publication',
@@ -21,15 +22,22 @@ const ClassifiedRateSchema = new auditingSchema({
   },
 })
 
-ClassifiedRateSchema.pre('validate', function(next) {
+ClassifiedReleaseOrderSchema.pre('validate', function(next) {
   next()
 })
 
-ClassifiedRateSchema.methods = {
+ClassifiedReleaseOrderSchema.methods = {
   /* Model Instance Methods come here */
 }
 
 /* Plug-ins */
-ClassifiedRateSchema.plugin(mongoosePaginate)
+ClassifiedReleaseOrderSchema.plugin(mongoosePaginate)
 
-export default mongoose.model('ClassifiedRate', ClassifiedRateSchema)
+ClassifiedReleaseOrderSchema.plugin(uniqueValidator, {
+  message: 'Classified Release Order  : "{VALUE}" already exists in system',
+})
+
+export default mongoose.model(
+  'ClassifiedReleaseOrder',
+  ClassifiedReleaseOrderSchema
+)
